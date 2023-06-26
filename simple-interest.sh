9835518f24cf2a8d8e6109e10095f741ddffa816
Lalman Thakur <90516185+Lalman888@users.noreply.github.com>
2023-06-26 11:57:24 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Lalman
    # Additional Authors:
-   # <your GitHub username>
+   # Lalman888
    # Input:
    # p, principal amount
    # t, time period in years
